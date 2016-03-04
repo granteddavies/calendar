368adc95a873db16807d6fa2d89c27dc5439916c
granteddavies <granteddavies@gmail.com>
2016-03-03 21:04:48 -0600

    Created the necessary calendar and list views.

--- app/src/main/java/com/calendar/MainActivity.java
+++ app/src/main/java/com/calendar/MainActivity.java
@@ -2,12 +2,24 @@ package com.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setShowWeekNumber(false);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
     }
 }
